942416a2602587dc3c3187c40426f062275977ba
Sravan kumar Velanati <sravanworkinfo@gmail.com>
2020-12-22 11:47:52 +0530

    order bug fix

--- app/src/main/java/com/jaldeeinc/jaldee/connection/ApiClient.java
+++ app/src/main/java/com/jaldeeinc/jaldee/connection/ApiClient.java
@@ -23,7 +23,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
 
 
-    public static final String BASE_URL = "https://www.jaldee.com/v1/rest/";
+    public static final String BASE_URL = "https://scale.jaldee.com/v1/rest/";
 
 
     private static Retrofit retrofit = null;
