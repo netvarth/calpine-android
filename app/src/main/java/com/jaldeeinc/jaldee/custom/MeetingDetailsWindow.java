0cbbaec864756e8a84003cee53f967eb34ea0ccd
Sravan kumar Velanati <sravanworkinfo@gmail.com>
2021-05-06 13:32:55 +0530

    bug fixes

--- app/src/main/java/com/jaldeeinc/jaldee/custom/MeetingDetailsWindow.java
+++ app/src/main/java/com/jaldeeinc/jaldee/custom/MeetingDetailsWindow.java
@@ -14,23 +14,24 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 
 import com.jaldeeinc.jaldee.R;
+import com.jaldeeinc.jaldee.activities.WebViewActivity;
 import com.jaldeeinc.jaldee.response.TeleServiceCheckIn;
 import com.jaldeeinc.jaldee.utils.SharedPreference;
 
 public class MeetingDetailsWindow extends Dialog {
 
-    TextView tvServiceName,tvTime,tvLink;
+    TextView tvServiceName, tvTime, tvLink;
     Button btJoin;
     String time = null;
     String serviceName, callingMode;
     TeleServiceCheckIn teleServiceCheckInResponse;
     Context context;
-    String virtualServiceType,countryCode;
+    String virtualServiceType, countryCode;
     ImageView ivClose;
 
     public MeetingDetailsWindow(@NonNull Context context, String checkInTime, String name, TeleServiceCheckIn teleServiceCheckInResponse, String callingMode, String countryCode) {
         super(context);
-        this.context =context;
+        this.context = context;
         this.time = checkInTime;
         this.serviceName = name;
         this.teleServiceCheckInResponse = teleServiceCheckInResponse;
@@ -61,13 +62,13 @@ public class MeetingDetailsWindow extends Dialog {
             }
         });
 
-        if (serviceName != null){
+        if (serviceName != null) {
             String name = serviceName;
 //            name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
             tvServiceName.setText(name);
 
             try {
-                if(callingMode!=null) {
+                if (callingMode != null) {
 
                     if (callingMode.equalsIgnoreCase("Zoom")) {
                         tvServiceName.setCompoundDrawablesWithIntrinsicBounds(R.drawable.zoom, 0, 0, 0);
@@ -78,19 +79,16 @@ public class MeetingDetailsWindow extends Dialog {
                     } else if (callingMode.equalsIgnoreCase("WhatsApp")) {
                         if (virtualServiceType != null && virtualServiceType.equalsIgnoreCase("videoService")) {
                             tvServiceName.setCompoundDrawablesWithIntrinsicBounds(R.drawable.whatsapp_videoicon, 0, 0, 0);
-                        }
-                        else {
+                        } else {
                             tvServiceName.setCompoundDrawablesWithIntrinsicBounds(R.drawable.whatsapp, 0, 0, 0);
                         }
                         tvServiceName.setCompoundDrawablePadding(15);
                     } else if (callingMode.equalsIgnoreCase("phone")) {
                         tvServiceName.setCompoundDrawablesWithIntrinsicBounds(R.drawable.phoneicon_sized, 0, 0, 0);
                         tvServiceName.setCompoundDrawablePadding(15);
-                    }
-                    else if (callingMode.equalsIgnoreCase("VideoCall")) {
+                    } else if (callingMode.equalsIgnoreCase("VideoCall")) {
                         tvServiceName.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_jaldeevideo, 0, 0, 0);
                         tvServiceName.setCompoundDrawablePadding(15);
-                        btJoin.setVisibility(View.GONE);
                     }
                 }
 
@@ -100,39 +98,41 @@ public class MeetingDetailsWindow extends Dialog {
 
         }
 
-        if (time != null){
+        if (time != null) {
             tvTime.setText(time);
         }
 
-        if (teleServiceCheckInResponse != null){
+        if (teleServiceCheckInResponse != null) {
 
-            if (teleServiceCheckInResponse.getJoiningUrl() != null){
+            if (teleServiceCheckInResponse.getJoiningUrl() != null) {
 
                 tvLink.setText(teleServiceCheckInResponse.getJoiningUrl());
-            }
-            else if (teleServiceCheckInResponse.getStartingUl() != null){
+            } else if (teleServiceCheckInResponse.getStartingUl() != null) {
 
                 tvLink.setText(teleServiceCheckInResponse.getStartingUl());
-            }
-            else{
+            } else {
                 btJoin.setVisibility(View.GONE);
             }
 
             tvLink.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent intent = new Intent();
-                    intent.setAction(Intent.ACTION_VIEW);
-                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                    if(tvLink!=null && callingMode!=null){
-                        String pass = SharedPreference.getInstance(context).getStringValue("password","");
-                        if (callingMode.equalsIgnoreCase("VideoCall")) {
-                            intent.setData(Uri.parse(tvLink.getText().toString() + "?pwd=" + pass));
-                        } else {
+
+                    if (tvLink != null && callingMode != null && callingMode.equalsIgnoreCase("VideoCall")) {
+                        Intent intent = new Intent(context, WebViewActivity.class);
+                        String pass = SharedPreference.getInstance(context).getStringValue("password", "");
+                        intent.putExtra("URL",tvLink.getText().toString() + "?pwd=" + pass);
+                        context.startActivity(intent);
+                    } else {
+
+                        Intent intent = new Intent();
+                        intent.setAction(Intent.ACTION_VIEW);
+                        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                        if (tvLink != null) {
                             intent.setData(Uri.parse(tvLink.getText().toString()));
                         }
+                        context.startActivity(intent);
                     }
-                    context.startActivity(intent);
                 }
             });
         }
@@ -140,11 +140,21 @@ public class MeetingDetailsWindow extends Dialog {
         btJoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse(tvLink.getText().toString()));
-                context.startActivity(intent);
+                if (tvLink != null && callingMode != null && callingMode.equalsIgnoreCase("VideoCall")) {
+                    Intent intent = new Intent(context, WebViewActivity.class);
+                    String pass = SharedPreference.getInstance(context).getStringValue("password", "");
+                    intent.putExtra("URL",tvLink.getText().toString() + "?pwd=" + pass);
+                    context.startActivity(intent);
+                } else {
+
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_VIEW);
+                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                    if (tvLink != null) {
+                        intent.setData(Uri.parse(tvLink.getText().toString()));
+                    }
+                    context.startActivity(intent);
+                }
             }
         });
 
