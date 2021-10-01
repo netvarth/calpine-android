3ca31053275c8019c2c4926a5f002a3ffaa62b0f
ajeesh.anand@netvarth.com <ajeesh.anand@netvarth.com>
2021-10-01 10:44:51 +0530

    Bug 12561

--- app/src/main/java/com/jaldeeinc/jaldee/custom/PrescriptionDialog.java
+++ app/src/main/java/com/jaldeeinc/jaldee/custom/PrescriptionDialog.java
@@ -33,7 +33,7 @@ public class PrescriptionDialog extends Dialog {
     private ActiveCheckIn checkInInfo;
     private ActiveAppointment appointmentInfo;
     private String type = "";
-    private String url ="";
+    private String url = "";
     private String longUrl = "";
 
 
@@ -68,83 +68,95 @@ public class PrescriptionDialog extends Dialog {
         tv_download = findViewById(R.id.presc_download);
         tv_share = findViewById(R.id.presc_share);
 
-
-        if(bookings.getCheckInInfo()!=null && bookings.getCheckInInfo().getPrescShortUrl()!=null){
-            url = bookings.getCheckInInfo().getPrescShortUrl();
+        if (bookings.getCheckInInfo() != null) {
+            if (bookings.getCheckInInfo().getPrescShortUrl() != null) {
+                url = bookings.getCheckInInfo().getPrescShortUrl();
+            }
             if (bookings.getCheckInInfo().getPrescUrl() != null) {
                 longUrl = bookings.getCheckInInfo().getPrescUrl();
             }
-        }
-        else if(bookings.getAppointmentInfo()!=null && bookings.getAppointmentInfo().getPrescShortUrl()!=null){
-            url = bookings.getAppointmentInfo().getPrescShortUrl();
+        } else if (bookings.getAppointmentInfo() != null) {
+            if (bookings.getAppointmentInfo().getPrescShortUrl() != null) {
+                url = bookings.getAppointmentInfo().getPrescShortUrl();
+            }
             if (bookings.getAppointmentInfo().getPrescUrl() != null) {
                 longUrl = bookings.getAppointmentInfo().getPrescUrl();
             }
-        }
-        else if(checkInInfo!=null && checkInInfo.getPrescShortUrl()!=null){
-            url = checkInInfo.getPrescShortUrl();
+        } else if (checkInInfo != null) {
+            if (checkInInfo.getPrescShortUrl() != null) {
+                url = checkInInfo.getPrescShortUrl();
+            }
             if (checkInInfo.getPrescUrl() != null) {
                 longUrl = checkInInfo.getPrescUrl();
             }
-        }
-        else if(appointmentInfo!=null && appointmentInfo.getPrescShortUrl()!=null){
-            url = appointmentInfo.getPrescShortUrl();
+        } else if (appointmentInfo != null) {
+            if (appointmentInfo.getPrescShortUrl() != null) {
+                url = appointmentInfo.getPrescShortUrl();
+            }
             if (appointmentInfo.getPrescUrl() != null) {
                 longUrl = appointmentInfo.getPrescUrl();
             }
         }
 
-
         tv_share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                       if(url!=null && !url.equalsIgnoreCase("")) {
-                           Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-                           sharingIntent.setType("text/html");
-                           String shareBody = url;
-                           sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Prescription details");
-                           sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
-                           view.getContext().startActivity(Intent.createChooser(sharingIntent, "Share via"));
-                       }
+                if (url != null && !url.equalsIgnoreCase("")) {
+                    sharePrescription(url, view);
+                } else if (longUrl != null && !longUrl.equalsIgnoreCase("")) {
+                    sharePrescription(longUrl, view);
+                }
             }
         });
-
 
         tv_download.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (url != null && !url.equalsIgnoreCase("")){
-                    if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions((Activity) unwrap(view.getContext()), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                        // this will request for permission when permission is not true
-                    } else {
-                        // Download code here
-
-                        File file = new File(Uri.parse(url).toString());
-                        Uri uri = Uri.parse(url);
-                        DownloadManager.Request request = new DownloadManager.Request(uri);
-                        request.setDescription(file.getName());
-                        request.setTitle(file.getName());
-                        request.setMimeType(getMimeType(longUrl));
-//                        request.setMimeType("application/pdf");
-//                        request.setMimeType("image/jpeg");
-                        // in order for this if to run, you must use the android 3.2 to compile your app
-                        request.allowScanningByMediaScanner();
-                        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, url);
-
-// get download service and enqueue file
-                        DownloadManager manager = (DownloadManager) mContext.getSystemService(Context.DOWNLOAD_SERVICE);
-                        if (manager != null) {
-                            manager.enqueue(request);
-                        }
-
-                    }
-            }
+                if (url != null && !url.equalsIgnoreCase("")) {
+                    downloadPrescription(url, longUrl, mContext, view);
+                } else if (longUrl != null && !longUrl.equalsIgnoreCase("")) {
+                    downloadPrescription(longUrl, longUrl, mContext, view);
+                }
             }
         });
+    }
 
+    public static void sharePrescription(String sUrl, View view) {
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/html");
+        String shareBody = sUrl;
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Prescription details");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+        view.getContext().startActivity(Intent.createChooser(sharingIntent, "Share via"));
+    }
+
+    public static void downloadPrescription(String url, String longUrl, Context mContext, View view) {
+        if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((Activity) unwrap(view.getContext()), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            // this will request for permission when permission is not true
+        } else {
+            // Download code here
+
+            File file = new File(Uri.parse(url).toString());
+            Uri uri = Uri.parse(url);
+            DownloadManager.Request request = new DownloadManager.Request(uri);
+            request.setDescription(file.getName());
+            request.setTitle(file.getName());
+            request.setMimeType(getMimeType(longUrl));
+//                        request.setMimeType("application/pdf");
+//                        request.setMimeType("image/jpeg");
+            // in order for this if to run, you must use the android 3.2 to compile your app
+            request.allowScanningByMediaScanner();
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, url);
+
+// get download service and enqueue file
+            DownloadManager manager = (DownloadManager) mContext.getSystemService(Context.DOWNLOAD_SERVICE);
+            if (manager != null) {
+                manager.enqueue(request);
+            }
+        }
     }
 
     public static String getMimeType(String url) {
@@ -160,8 +172,6 @@ public class PrescriptionDialog extends Dialog {
         while (!(context instanceof Activity) && context instanceof ContextWrapper) {
             context = ((ContextWrapper) context).getBaseContext();
         }
-
         return (Activity) context;
     }
-
 }
